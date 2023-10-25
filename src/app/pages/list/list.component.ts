8177b949b76e550807d44e20621ffbe51987d0f1
AntonioPortada <antonio_portada25@hotmail.com>
2023-10-24 20:02:08 -0600

    cartas desde service

---
+++ src/app/pages/list/list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Card } from 'src/app/interfaces/card.interface';
+import { CardService } from 'src/app/services/card.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  cards: Card[] = []
+
+  constructor(private cardService: CardService) { }
+
+  ngOnInit(): void {
+    this.cardService.getCards().subscribe(res => {
+      console.log(res);
+      this.cards = res
+    });
+  }
+
+}
